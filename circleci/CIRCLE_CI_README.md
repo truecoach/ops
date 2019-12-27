0c7dc1adfeb7906416562661a84b3cd63d7c1bab
Adam Steel <adamgsteel@gmail.com>
2019-12-27 08:35:50 -0700

    [feature] add CircleCI scripts

---
+++ circleci/CIRCLE_CI_README.md
@@ -0,0 +1,4 @@
+# CircleCI Scripts
+
+A collection of scripts to automake workflow based on CircleCI builds. Some functionality
+is dependent on CircleCI specific things.
---
+++ circleci/circle_ci_git_inspector.rb
@@ -0,0 +1,54 @@
+class CircleCiGitInspector
+  DEVELOP_BRANCH = 'develop'
+  MASTER_BRANCH = 'master'
+
+  def deployed?
+    current_branch == MASTER_BRANCH
+  end
+
+  def merged?
+    current_branch == DEVELOP_BRANCH
+  end
+
+  def pr_exists?
+    !ENV['CIRCLE_PULL_REQUEST'].nil?
+  end
+
+  def titles
+    commit_messages(git_opts: ['--pretty=format:"%s"']).split("\n")
+  end
+
+  def commit_messages(git_opts: [])
+    @_commit_messages ||= begin
+      return git_log_master_messages(git_opts) if deployed?
+      return git_log('origin/master..origin/develop', opts: git_opts) if merged?
+
+      git_log("origin/develop..#{current_branch}", opts: git_opts)
+    rescue Exception => e
+      puts ""
+      puts "ERROR: Failed to parse commit messages: #{e.message}"
+      puts ""
+      ""
+    end
+  end
+
+  def git_log_master_messages(git_opts)
+    # attempts to read commits messages between most recent master SHA
+    # and SHA of last master build in Circle
+    left, right = compare_url[/compare\/.*\.\.\..*$/].gsub('compare/', '').split('...')
+
+    git_log("#{left}...#{right}", opts: git_opts)
+  end
+
+  def git_log(diff, opts: [])
+    `git log #{diff} --no-merges #{opts.join(' ')}`.chomp
+  end
+
+  def compare_url
+    ENV['CIRCLE_COMPARE_URL'].to_s
+  end
+
+  def current_branch
+    @_current_branch ||= `git rev-parse --abbrev-ref HEAD`.chomp
+  end
+end
